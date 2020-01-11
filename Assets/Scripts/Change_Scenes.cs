3068fe894a8e13d165a6a5d52864e228f63cc83e
Corey <coreydknighten@yahoo.com>
2020-01-10 19:56:03 -0500

    Update Change_Scenes.cs
    Updated the Change_Scenes script.

--- Assets/Scripts/Change_Scenes.cs
+++ Assets/Scripts/Change_Scenes.cs
@@ -3,30 +3,48 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class Change_Scenes : MonoBehaviour
-{  
-    [SerializeField]
-    public int essence = 3;
-     
-
-    void OnTriggerEnter(Collider other)
+// Implementing Namespace Knighten
+namespace Knighten
+{
+    // This class Changes the Scenes once all essence(Collectables) are collected.
+    public class Change_Scenes : MonoBehaviour
     {
-        if (other.gameObject.CompareTag("Collectables"))
-        {
-            Debug.Log(" llll");
-            
-            essence -= 1;
-            Debug.Log(essence);
+        // Member Variables
+        [SerializeField] private string nextScene;
+        public int essence = 3;
 
-            if (essence == 0)
+        // Upon collision with a collectable, that collectable
+        // is deducted from the (essence) variable.
+        void OnTriggerEnter(Collider other)
+        {
+            // If the other objects's tag is Collectables
+            // execute the following code.
+            if (other.gameObject.CompareTag("Collectables"))
             {
-                Debug.Log(" Simple");
-                SceneManager.LoadScene("Asteria Boss Scene");
+                // Deduct 1 essence from the starting amount.
+                essence -= 1;
+
+                // Show the remaining number of essence in the console.
+                Debug.Log(essence);
+
+                // If all 3 collectables are found
+                // change the scene to the giving scene name.
+                if (essence == 0)
+                {
+                    SceneManager.LoadScene(nextScene);
+                }
             }
         }
+
+        /*
+         * Main Menu Ideas    
+         *    
+         * var sceneName : string = "MainMenu";
+     var sceneIndex : int = 0;
+     Application.LoadLevel(sceneName); // Load level by name
+     Application.LoadLevel(sceneIndex); // Load level by index number   
+         */
+
+
     }
-
-
-
-
-}
+} 
